2d4d4624366df9de0ba447d1762e7baa333bc618
Lingnaaaa <lingnah794@deltalearns.ca>
2025-02-13 11:14:45 -0800

    I finished

--- assignment4.py
+++ assignment4.py
@@ -1,16 +1,16 @@
 def title():
     print('Welcome to globular Interest calculator')
     print('You can calculate Interest volume')
-    print('You can test this program by (p,r,n,t)=(1000,4,4,2)')
+    print('You can test this program by (p,r,n,t)=(1000,4,4,2)\nThe answer should be 1082.86')
 
 def main():
     title()
     while True:
         try:
           P=float(input("Enter your principal:"))
-          r=float(input('Enter your rate'))
-          n=float(input('Enter your # of compounding periods per year'))
-          t=float(input('Enter your # of year'))
+          r=float(input('Enter your rate:'))
+          n=float(input('Enter your # of compounding periods per year:'))
+          t=float(input('Enter your # of year:'))
           A=P*(1+(r/100)/n)**(n*t)
           A=round(A,2)
           print(A)
